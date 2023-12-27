367d234494d6cff8ed24b1c13e2af8c0f9a243bd
githubofkrishnadhas <krishnadhasnk1997@gmail.com>
2023-12-27 16:16:53 +0530

    DEVOPS-25 fixed typo in credential

--- configure_dependabot.sh
+++ configure_dependabot.sh
@@ -6,6 +6,6 @@ REPOSITORY_NAME=$2
 curl -L \
   -X PUT \
   -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer $GH_TOKEBN" \
+  -H "Authorization: Bearer $GH_TOKEN" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   https://api.github.com/repos/$ORGANIZATION/$REPOSITORY_NAME/automated-security-fixes
